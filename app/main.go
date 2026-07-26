ae1169f882ea5106a7a7d4d0922149ca5e184228
akane <706708950@qq.com>
2026-07-27 01:09:55 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,6 +10,22 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+func handle(conn net.Conn){
+	defer conn.Close()
+
+	buffer := make([]byte, 128)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil{
+			fmt.Println("Error reading from client: ", err.Error)
+			os.Exit(1)
+		}
+		fmt.Println("Receive from client: ", string(buffer[:n]))
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -26,15 +42,6 @@ func main() {
 	}
 
 	// Receive PING and reponse in hardcode
-	buffer := make([]byte, 64)
-	n, err := conn.Read(buffer)
-	if err != nil{
-		fmt.Println("Error reading from client: ", err.Error)
-		os.Exit(1)
-	}
-	fmt.Println("Receive from client: ", string(buffer[:n]))
+	go handle(conn)
 
-	conn.Write([]byte("+PONG\r\n"))
-
-	defer conn.Close()
 }
